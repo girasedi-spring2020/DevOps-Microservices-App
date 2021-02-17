7b26d48db4fbe7a090a000f12bd0a45b84271d21
Divya Vijendra Girase <girase.di@husky.neu.edu>
2021-02-17 18:24:42 -0500

    Added endpoints

--- sa-frontend/src/components/Polarity/Polarity.js
+++ sa-frontend/src/components/Polarity/Polarity.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Polarity extends Component {
   propTypes = {
-    sentence: PropTypes.string.isRequired, polarity: PropTypes.number.isRequired
+    sentence: PropTypes.string.isRequired,
+    polarity: PropTypes.number.isRequired,
   };
 
   render() {
-    const green = Math.round((this.props.polarity + 1) * 128); const red = 255 - green;
+    const green = Math.round((this.props.polarity + 1) * 128);
+    const red = 255 - green;
     const textColor = {
-      backgroundColor: 'rgb(' + red + ', ' + green + ', 0)', padding: '15px'
+      backgroundColor: "rgb(" + red + ", " + green + ", 0)",
+      padding: "15px",
     };
-    return <div style={textColor}>"{this.props.sentence}" has polarity of {this.props.polarity} </div>
+    return (
+      <div style={textColor}>
+        "{this.props.sentence}" has polarity of {this.props.polarity}{" "}
+      </div>
+    );
   }
 }
 
